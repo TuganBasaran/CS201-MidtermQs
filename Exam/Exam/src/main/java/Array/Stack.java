3fffe1a1dd5dcbade119ae191f350adba4668896
Basaran <tuganbasaran03@gmail.com>
2024-03-16 22:14:03 +0300

    final push

--- Exam/Exam/src/main/java/Array/Stack.java
+++ Exam/Exam/src/main/java/Array/Stack.java
@@ -37,4 +37,50 @@ public class Stack {
         }
         return null;
     }
+
+    /** Write a function using stacks that determines if a parenthesis sequence
+    is balanced or not. For example the parenthesis sequence ( ( ) ( ) ) is
+    balanced, whereas the parenthesis sequence ( ( ( ) ( is not. You can
+    assume that the character sequence contains just ’(’ and ’)’ characters. */
+    boolean isBalanced(String s){
+        Stack stack = new Stack(s.length());
+        for (int i = 0; i < s.length(); i++){
+            if (s.charAt(i) == '('){
+                stack.push(new Element(s.charAt(i)));
+            } else if (s.charAt(i) == ')'){
+                if (stack.isEmpty()){
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    /**  Write a function using stacks that determines if a parenthesis sequence
+    is balanced or not. For example the parenthesis sequence ( { ()[ { }
+    ] } ( ) ) is balanced, whereas the parenthesis sequence ( } ] ) ( is not.
+    You can assume that the character sequence contains just (, {, [, ), }, ] characters. */
+
+    boolean isBalanced2(String s){
+        Stack theSameStack = new Stack(s.length()); 
+        for(int i = 0; i <s.length(); i++){
+            if(s.charAt(i) == '(' || s.charAt(i)=='{' || s.charAt(i)== '['){
+                theSameStack.push(new Element(s.charAt(i)));
+            } else if(s.charAt(i)== ']' || s.charAt(i)== '}' || s.charAt(i) == ')'){
+                if(theSameStack.isEmpty()){
+                    return false; 
+                } else if (theSameStack.peek().getData() == '(' && s.charAt(i) == ')') {
+                    theSameStack.pop(); 
+                } else if (theSameStack.peek().getData() == '[' && s.charAt(i) == ']') {
+                    theSameStack.pop(); 
+                } else if (theSameStack.peek().getData() == '{' && s.charAt(i) == '}') {
+                    theSameStack.pop(); 
+                } else {
+                    return false; 
+                }
+            }
+        }
+        return theSameStack.isEmpty(); 
 }
